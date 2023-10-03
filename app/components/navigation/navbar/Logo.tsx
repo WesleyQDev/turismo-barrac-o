54a99b3ab88b96555c6e7818542a081730f5f6d5
CodeSandbox <support@codesandbox.io>
2023-10-03 16:05:53 +0000

    Initial commit

---
+++ app/components/navigation/navbar/Logo.tsx
@@ -0,0 +1,58 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Button from "./Button";
+
+const Logo = () => {
+  //update the size of the logo when the size of the screen changes
+  const [width, setWidth] = useState(0);
+
+  const updateWidth = () => {
+    const newWidth = window.innerWidth;
+    setWidth(newWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth);
+    updateWidth();
+  }, []);
+
+  // change between the logo and the button when the user scrolls
+  const [showButton, setShowButton] = useState(false);
+
+  const changeNavButton = () => {
+    if (window.scrollY >= 400 && window.innerWidth < 768) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNavButton);
+  }, []);
+
+  return (
+    <>
+      <Link href="/" style={{ display: showButton ? "none" : "block" }}>
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/2/23/Barrac%C3%A3obras%C3%A3o.png"
+          alt="Logo"
+          width={width < 1024 ? "55" : "55"}
+          height={width < 1024 ? "45" : "45"}
+          className="relative pr-3"
+        />
+      </Link>
+      <div
+        style={{
+          display: showButton ? "block" : "none",
+        }}
+      >
+        <Button />
+      </div>
+    </>
+  );
+};
+
+export default Logo;
