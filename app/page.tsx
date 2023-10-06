09c0856cf7223a47fccbd00c32f95c16937911f9
Wesley <146393663+WesleyQDev@users.noreply.github.com>
2023-10-06 19:52:51 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,12 @@ export default function Home() {
     <>
       <Banner />
       <div className="container">
-        <h2>Guia turística Barracão</h2>
+        <h2>Guia turístico Barracão</h2>
         <p>
-          Se você está de visita em Barracão uma das primeiras coisas a se fazer
-          é encontrar algum lugar para se fixar, nesta guia te mostraremos
-          algumas hospedarias e posteriormente lugares para visitar aqui na
-          nossa cidade, a ordem que vamos utilizar não é por avaliações, então
-          fique a vontade para escolher.
+          Se você está visitando Barracão uma das primeiras coisas a se fazer é
+          encontrar algum lugar para ficar, neste guia iremos lhe mostrar
+          algumas opções de hospedagem e pontos turisticos e receptivos do nosso
+          municipio.
         </p>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3578.2881473403963!2d-53.63732782458313!3d-26.252309977047062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f09a34679f895d%3A0xd99cfe554f595799!2sPrefeitura%20Municipal%20de%20Barrac%C3%A3o!5e0!3m2!1spt-BR!2sbr!4v1696619216833!5m2!1spt-BR!2sbr"
