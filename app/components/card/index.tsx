c99a6b5e6313be722395f77568f11a88604282ee
Wesley <146393663+WesleyQDev@users.noreply.github.com>
2023-10-07 17:37:59 +0000

    Update index.tsx and index.tsx

--- app/components/card/index.tsx
+++ app/components/card/index.tsx
@@ -1,29 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 import Maps from "../maps";
-export default function Card(props) {
+export default function Card(card) {
   return (
     <>
       <Link
         style={{ marginTop: 10, marginBottom: 10 }}
-        href={props.src}
+        href={card.src}
         className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
         <Image
           className="object-cover w-full rounded-t-lg h-96 md:h-100 md:w-45 md:rounded-none md:rounded-l-lg"
-          src={props.img}
-          alt={props.alt}
+          src={card.img}
+          alt={card.alt}
           width={0}
           height={0}
           sizes="100%"
         />
         <div className="flex flex-col justify-between p-4 leading-normal">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {props.nome}
+            {card.nome}
           </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {props.desc}
-            <Maps url={props.maps} />
+            {card.desc}
+            <Maps url={card.maps} />
           </p>
         </div>
       </Link>
--- app/components/maps/index.tsx
+++ app/components/maps/index.tsx
@@ -1,3 +1,3 @@
-export default function Maps(props) {
-  return <iframe src={props.url} loading="lazy" className="maps"></iframe>;
+export default function Maps(maps) {
+  return <iframe src={maps.url} loading="lazy" className="maps"></iframe>;
 }
